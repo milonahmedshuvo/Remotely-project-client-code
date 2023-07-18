af8e971254f064fd029cc57ba3dcef38c8a0de02
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-07-19 03:18:24 +0600

    btn style

--- src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/MyJobPost.jsx
+++ src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/MyJobPost.jsx
@@ -10,16 +10,18 @@ const MyJobPost = () => {
       }
 
 
-      const {data:jobposts=[], isLoading }= useQuery({
+      const {data:jobposts=[], isLoading, refetch }= useQuery({
         queryKey: ['jobposts', user?.email],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/getJobPostData?email=${user?.email}`);
             const data= await res.json()
             return data
+           
         }  
       })
        
       if(isLoading){
+        
         return <h1> my job post loading...</h1>
       }
 
@@ -35,6 +37,7 @@ const MyJobPost = () => {
             jobposts.map((posts, i) => <SingleJobPosts
             key={i}
             posts={posts}
+            refetch={refetch}
             ></SingleJobPosts>)
         }  
     </div>
--- src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/SingleJobPosts.jsx
+++ src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/SingleJobPosts.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const SingleJobPosts = ({posts}) => {
+const SingleJobPosts = ({ posts, refetch }) => {
+  console.log(posts);
+  const { address, company, drescription, jobPossition } = posts;
 
-    console.log(posts)
-    const {address, company, drescription, jobPossition } = posts
-
-
+  refetch();
   return (
     <div>
-          <div className='border border-gray-300 rounded mb-5 px-3 py-2'>
-              <p className='text-2xl '>Possition: {jobPossition} </p>
-              <p className='text-xl'>Company: {company} </p>
-              <p className='text-lg'>Address: {address} </p>
-              <p className='mt-2 text-md'>{drescription} </p>
+      <div className="border border-gray-300 rounded mb-5 px-3 py-2">
+        <div className="flex justify-between ">
+          <div>
+            <p className="text-2xl ">Possition: {jobPossition} </p>
+            <p className="text-xl">Company: {company} </p>
+            <p className="text-lg">Address: {address} </p>
           </div>
-    </div>
-  )
-}
 
-export default SingleJobPosts
+          <div className="flex ">
+            <button className="btn btn-success btn-outline btn-sm mr-2">publish</button>
+            <button className="btn btn-error btn-outline btn-sm mr-2 ">Delete</button>
+          </div>
+        </div>
+
+        <p className="mt-2 text-md">{drescription} </p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleJobPosts;
