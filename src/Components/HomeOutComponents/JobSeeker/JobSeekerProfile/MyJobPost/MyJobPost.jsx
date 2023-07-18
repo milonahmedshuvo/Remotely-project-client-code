f8a48228af223e67d0edcff8c70d0eaeab2fd609
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-07-19 02:31:26 +0600

    get job post query

--- src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/MyJobPost.jsx
+++ src/Components/HomeOutComponents/JobSeeker/JobSeekerProfile/MyJobPost/MyJobPost.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { createContextUser } from '../../../../Sheared/Context/FullAppContext'
+import { useQuery } from '@tanstack/react-query'
 
 const MyJobPost = () => {
+      const { user, loading }= useContext(createContextUser)
+      if(loading){
+        return <h1> my job post loading...</h1>
+      }
+
+
+      const {data:jobposts=[], isLoading }= useQuery({
+        queryKey: ['jobposts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/getJobPostData?email=${user?.email}`);
+            const data= await res.json()
+            return data
+        }  
+      })
+       
+      if(isLoading){
+        return <h1> my job post loading...</h1>
+      }
+
+      console.log("jobposts data", jobposts)
+       
 
   return (
     <div>
