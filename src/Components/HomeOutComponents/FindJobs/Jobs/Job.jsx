377a03ca0dac4dfef257c61a2584c4efabbf4796
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-07-27 23:55:41 +0600

    company

--- src/Components/HomeOutComponents/FindJobs/Jobs/Job.jsx
+++ src/Components/HomeOutComponents/FindJobs/Jobs/Job.jsx
@@ -12,6 +12,7 @@ import {
     FaUniversity,
     FaPeopleArrows,
     FaRegHeart,
+    FaCheckCircle
   } from "react-icons/fa";
   import { Link } from "react-router-dom";
 
@@ -38,18 +39,36 @@ const Job = ({ job }) => {
   return (
     <div className="bg-[#F4FCFF] px-3 pt-6 pb-10 my-2 rounded">
       <div className="flex justify-between">
-        <h2 className="text-[#007CD8] font-semibold text-lg">{jobTitle}</h2>
+        <h2 className="text-[#2196F3] font-semibold text-lg">{jobTitle}</h2>
         <FaRegStar className="text-[#46AC49] text-[30px]"></FaRegStar>
       </div>
 
-      <div className="flex items-center ">
+
+        <div className="flex items-center" >
+            <h1 className='text-[#2196F3] text-xl mr-2'>{companyName} </h1>
+            <FaCheckCircle className="text-[#2196F3]"></FaCheckCircle>
+        </div>
+
+      
+
+      <div className="flex  mb-4">
+          
+      <div className="flex items-center mr-10 ">
         <FaAirbnb className="text-lg mr-[2px] "></FaAirbnb>
         <p className="">{location}</p>
       </div>
-      <div className="mb-4  flex items-center ">
+      <div className="flex items-center ">
         <FaUniversity className="mr-[2px]"></FaUniversity>
         <p>{jobtype}</p>
       </div>
+
+
+      </div>
+
+
+
+
+
 
       <p className="text-sm mb-1 ">{drescription}...</p>
 
