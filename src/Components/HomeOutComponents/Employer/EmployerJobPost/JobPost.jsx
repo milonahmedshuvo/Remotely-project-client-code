305ecf316eccd9576135652dbef49b10be6f1ece
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-07-30 19:07:38 +0600

    delete functionality

--- src/Components/HomeOutComponents/Employer/EmployerJobPost/JobPost.jsx
+++ src/Components/HomeOutComponents/Employer/EmployerJobPost/JobPost.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React from "react";
+import { toast } from "react-hot-toast";
 
-const JobPost = ({post}) => {
- const {address,company,drescription,jobPossition,skills} =post
+const JobPost = ({ post }) => {
+  const { address, company, drescription, jobPossition, skills, _id } = post;
+
+  const handleEmployerpostDelete = (id) => {
+    fetch(`http://localhost:5000/employerPostDelete/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        toast.success("Succesful Delete");
+      })
+      .catch((err) => {
+        toast.error("Filed Delete");
+      });
+  };
 
   return (
-    <div className='mx-4 my-16'>
-          <div>
-            <p className='text-2xl'>{jobPossition}</p>
-            <p className='text-xl'>{company}</p>
-            <p className='text-xl'>{address}</p>
-          </div>
+    <div className="mx-4 my-16">
+      <div>
+        <p className="text-2xl">{jobPossition}</p>
+        <p className="text-xl">{company}</p>
+        <p className="text-xl">{address}</p>
+      </div>
 
-            <p className='mt-2 '>{drescription}</p>
+      <p className="mt-2 ">{drescription}</p>
 
-            <button className="btn btn-outline btn-error font-bold mt-2 btn-sm">Delete</button>
+      <button
+        onClick={() => handleEmployerpostDelete(_id)}
+        className="btn btn-outline btn-error font-bold mt-2 btn-sm"
+      >
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default JobPost
-
-
-  
+export default JobPost;
