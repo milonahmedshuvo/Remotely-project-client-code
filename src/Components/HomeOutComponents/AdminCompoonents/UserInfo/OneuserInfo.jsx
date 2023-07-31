5165bb631becb91525b9ae53e9d0b18513d202c0
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-07-31 23:53:01 +0600

    delete components

--- src/Components/HomeOutComponents/AdminCompoonents/UserInfo/OneuserInfo.jsx
+++ src/Components/HomeOutComponents/AdminCompoonents/UserInfo/OneuserInfo.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import { toast } from 'react-hot-toast';
+import React from "react";
+import { toast } from "react-hot-toast";
 
-const OneuserInfo = ({user}) => {
-      console.log(user)
-      const {image,userIdentity,userName,_id} = user;
+const OneuserInfo = ({ user }) => {
+  console.log(user);
+  const { image, userIdentity, userName, _id } = user;
 
-      const handleUserDelete = (id) => {
-        console.log("click")
-        fetch(`http://localhost:5000/userDelete/${id}`)
-        .then((res)=> res.json())
-        .then((data)=> {
-          console.log(data)
-          toast.success("Delete Successful")
-        })
-        .catch((err)=> {
-          toast.error("Filed delete")
-        })
-      }
+  const handleUserDelete = (id) => {
+    console.log("click");
+    fetch(`http://localhost:5000/userDelete/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        toast.success("Delete Successful");
+      })
+      .catch((err) => {
+        toast.error("Filed delete");
+      });
+  };
 
   return (
-    <div className='grid grid-cols-2 md:grid-cols-4 my-4 '>
-         <img
-          className="rounded-full  h-10 w-10 mx-auto "
-          src={image} alt="" />
+    <div className="grid grid-cols-2 md:grid-cols-4 my-4 ">
+      <img className="rounded-full  h-10 w-10 mx-auto " src={image} alt="" />
 
-        <div>
-        <p className='text-center'>{userName}</p>
-        </div>
+      <div>
+        <p className="text-center">{userName}</p>
+      </div>
 
-          
-          <div>
-          <p className='text-center'>{userIdentity}</p>
-          </div>
+      <div>
+        <p className="text-center">{userIdentity}</p>
+      </div>
 
-          <div className='mx-auto'>
-          <button 
-           onClick={()=> handleUserDelete(_id)}
-          className="btn btn-sm btn-error">Delete</button>
-          </div>
+      <div className="mx-auto">
+        <button
+          onClick={() => handleUserDelete(_id)}
+          className="btn btn-sm btn-error"
+        >
+          Delete
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OneuserInfo
+export default OneuserInfo;
