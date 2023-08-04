71eeaabaf60b2cc40ecd5c3de6ec9baaf12ef88e
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-08-05 03:02:46 +0600

    footer complete

--- src/Components/Sheared/Footer/Footer.jsx
+++ src/Components/Sheared/Footer/Footer.jsx
@@ -3,53 +3,57 @@ import React from "react";
 const Footer = () => {
   return (
     <div >
-      <footer className="footer p-10 bg-[#222E39] text-white py-14 ">
+      <footer className="footer p-10 bg-[#1F69CA] text-white py-14 ">
         <div>
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clip-rule="evenodd"
-            className="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
-          </p>
-        </div>
-        <div className="text-white">
-          <span className="footer-title ">About Remotely</span>
-          <a className="link link-hover text-white"> About Us</a>
-          <a className="link link-hover text-white">Contact Us</a>
-          <a className="link link-hover text-white">Guidelines</a>
-          <a className="link link-hover text-white">Privacy Policy</a>
-          <a className="link link-hover text-white">Cookie Policy</a>
-          <a className="link link-hover text-white">Terms & Conditions</a>
-          <a className="link link-hover text-white">Sitemap</a>
-        </div>
-        <div>
-          <span className="footer-title">For Jobseekers</span>
-          <a className="link link-hover text-white">Browse Companies</a>
-          <a className="link link-hover text-white">Explore Internships</a>
-          <a className="link link-hover text-white">Upload Resume</a>
-          <a className="link link-hover text-white">Give Company Review</a>
-        </div>
-        <div>
-          <span className="footer-title text-white">For Employers</span>
-          <a className="link link-hover text-white">Post a Job for free</a>
-          <a className="link link-hover text-white">Hire Talent</a>
-          <a className="link link-hover text-white">Login</a>
-          <a className="link link-hover text-white">Sign Up</a>
+            <img src="https://softzino.com/images/icon-white.png" alt="" />
+            <p className="text-[16px] mt-4">Since our establishment, we have</p>
+            <p className="text-[16px] ">been delivering high-quality and</p>
+            <p className="text-[16px]">sustainable software solutions for</p>
+            <p className="text-[16px]">corporate purposes of worldwide</p>
+            <p className="text-[16px]">businesses.</p>
+
+
+            <div className="mt-10">
+              <img src="https://softzino.com/images/basis.jpg" alt="" />
+            </div>
         </div>
 
+
+        <div>
+          <span className=" font-medium text-white text-xl">For Jobseekers</span>
+          <a className="link link-hover text-white text-lg">Browse Companies</a>
+          <a className="link link-hover text-lg">Explore Internships</a>
+          <a className="link link-hover text-lg">Upload Resume</a>
+          <a className="link link-hover text-lg">Give Company Review</a>
+        </div>
 
         
+        
+        <div>
+          <span className="font-medium  text-xl">For Employers</span>
+          <a className="link link-hover text-lg">Post a Job for free</a>
+          <a className="link link-hover text-lg">Hire Talent</a>
+          <a className="link link-hover text-lg">Login</a>
+          <a className="link link-hover text-lg">Sign Up</a>
+        </div>
+
+
+        <div className="text-lg">
+          <span className="font-medium  text-xl ">About Remotely</span>
+          <a className="link link-hover text-lg"> About Us</a>
+          <a className="link link-hover text-lg">Contact Us</a>
+          <a className="link link-hover text-lg">Guidelines</a>
+          <a className="link link-hover text-lg">Privacy Policy</a>
+          <a className="link link-hover text-lg">Cookie Policy</a>
+          <a className="link link-hover text-lg">Terms & Conditions</a>
+          <a className="link link-hover text-lg">Sitemap</a>
+        </div>
+
+        
+        
       </footer>
+        
+      
 
       
     </div>
