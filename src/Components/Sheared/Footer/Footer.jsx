b2841e015b96b68aed6e9a9d423f83cee69ca805
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-08-05 03:41:43 +0600

    success projects

--- src/Components/Sheared/Footer/Footer.jsx
+++ src/Components/Sheared/Footer/Footer.jsx
@@ -2,33 +2,31 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div >
+    <div>
       <footer className="footer p-10 bg-[#1F69CA] text-white pt-14 ">
         <div>
-            <img src="https://softzino.com/images/icon-white.png" alt="" />
-            <p className="text-[16px] mt-4">Since our establishment, we have</p>
-            <p className="text-[16px] ">been delivering high-quality and</p>
-            <p className="text-[16px]">sustainable software solutions for</p>
-            <p className="text-[16px]">corporate purposes of worldwide</p>
-            <p className="text-[16px]">businesses.</p>
+          <img src="https://softzino.com/images/icon-white.png" alt="" />
+          <p className="text-[16px] mt-4">Since our establishment, we have</p>
+          <p className="text-[16px] ">been delivering high-quality and</p>
+          <p className="text-[16px]">sustainable software solutions for</p>
+          <p className="text-[16px]">corporate purposes of worldwide</p>
+          <p className="text-[16px]">businesses.</p>
 
-
-            <div className="mt-10">
-              <img src="https://softzino.com/images/basis.jpg" alt="" />
-            </div>
+          <div className="mt-10">
+            <img src="https://softzino.com/images/basis.jpg" alt="" />
+          </div>
         </div>
 
-
         <div>
-          <span className=" font-medium text-white text-xl">For Jobseekers</span>
+          <span className=" font-medium text-white text-xl">
+            For Jobseekers
+          </span>
           <a className="link link-hover text-white text-lg">Browse Companies</a>
           <a className="link link-hover text-lg">Explore Internships</a>
           <a className="link link-hover text-lg">Upload Resume</a>
           <a className="link link-hover text-lg">Give Company Review</a>
         </div>
 
-        
-        
         <div>
           <span className="font-medium  text-xl">For Employers</span>
           <a className="link link-hover text-lg">Post a Job for free</a>
@@ -36,7 +34,6 @@ const Footer = () => {
           <a className="link link-hover text-lg">Login</a>
           <a className="link link-hover text-lg">Sign Up</a>
         </div>
-
 
         <div className="text-lg">
           <span className="font-medium  text-xl ">About Remotely</span>
@@ -48,18 +45,14 @@ const Footer = () => {
           <a className="link link-hover text-lg">Terms & Conditions</a>
           <a className="link link-hover text-lg">Sitemap</a>
         </div>
-
-        
-        
       </footer>
-        
-      
-       <div className="bg-[#1F69CA] pb-20">
-        <hr></hr>
-        <h1 className="font-semibold text-center text-lg text-white mt-3">Copyright © 2023 Softzino Technologies</h1>
 
-       </div>
-      
+      <div className="bg-[#1F69CA] pb-20">
+        <hr></hr>
+        <h1 className="font-semibold text-center text-lg text-white mt-3">
+          Copyright © 2023 Softzino Technologies
+        </h1>
+      </div>
     </div>
   );
 };
